fc996d025a38c4cda1edc5e22172601aa31e1728
priyadarshi prince <priyadarshiprince5@gmail.com>
2026-02-17 00:01:43 +0530

    add razorpay

--- backend/config/razorpay.js
+++ backend/config/razorpay.js
@@ -1,8 +1,14 @@
 const Razorpay = require("razorpay");
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+module.exports = function getRazorpayInstance() {
 
-module.exports = razorpay;
+  if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+    console.log("❌ Razorpay ENV missing");
+    throw new Error("Razorpay keys not configured");
+  }
+
+  return new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
+};
