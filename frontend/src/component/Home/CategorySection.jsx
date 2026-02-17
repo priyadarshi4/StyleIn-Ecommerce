92be5e770964a6c4467574c8b9ebee4e9be7620b
priyadarshi prince <priyadarshiprince5@gmail.com>
2026-02-18 02:53:25 +0530

    home ui update

--- frontend/src/component/Home/CategorySection.jsx
+++ frontend/src/component/Home/CategorySection.jsx
@@ -7,24 +7,30 @@ import "./CategorySection.css";
 function CategorySection() {
   return (
     <div className="category_wrapper">
-      <motion.div
-        className="category_section"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-      >
-        {categories.map((cat) => (
-          <Link
-            key={cat.name}
-            to={`/products?category=${encodeURIComponent(cat.name)}`}
-            className="category_card"
-          >
-            <div className="category_icon">
-              <img src={cat.image} alt={cat.name} />
-            </div>
-            <p>{cat.name}</p>
-          </Link>
-        ))}
-      </motion.div>
+      <div className="category_container_card">
+
+        <h3 className="category_title">CATEGORIES</h3>
+
+        <motion.div
+          className="category_section"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          {categories.map((cat) => (
+            <Link
+              key={cat.name}
+              to={`/products?category=${encodeURIComponent(cat.name)}`}
+              className="category_card"
+            >
+              <div className="category_icon">
+                <img src={cat.image} alt={cat.name} />
+              </div>
+              <p>{cat.name}</p>
+            </Link>
+          ))}
+        </motion.div>
+
+      </div>
     </div>
   );
 }
